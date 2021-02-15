f28d230fde6de456386be91c4265076dc0f1ac8f
natig0n <natigon87@hotmail.com>
2021-02-15 12:22:14 -0300

    Created BE solution

---
+++ accounting-api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace accounting_api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
